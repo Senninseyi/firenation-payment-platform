ea5be4eb963bdd223379b0d0f6f8ad270bb8b297
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-08 15:15:37 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,8 @@
 import { Cart } from "./icons/cart";
 import { Logo } from "./icons/logo";
 import { Menu } from "./icons/menu";
+import { Add } from "./icons/add";
+import { Subtract } from "./icons/subtract";
+import { Delete } from "./icons/delete";
 
-export { Cart, Logo, Menu };
+export { Cart, Logo, Menu, Add, Subtract, Delete };
