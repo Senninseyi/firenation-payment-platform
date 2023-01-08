070deea0ea0c142368ea26b331f309cb36a8ea3d
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-08 09:00:59 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { Footer } from "./footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-full flex items-center flex-col justify-center">
+    <div className="w-full flex items-center h-full flex-col justify-center">
       <div className="w-full max-w-[105rem]">
         <Nav />
         <main className="px-8 py-3 w-full h-full min-h-screen bg-secondary">
