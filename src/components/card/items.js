ee493b3da3e1ddf1f70134b69652c1acdc7c8f1f
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-20 11:29:58 +0100

    Update items.js

--- src/components/card/items.js
+++ src/components/card/items.js
@@ -1,30 +1,25 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addCart, selectedItem } from "../../app/slices/cart";
+import { addCart, selectedItem, updatePrice } from "../../app/slices/cart";
 
 function Items({ id, title, price, image }) {
-  const cart = useSelector((state) => state.carts);
-
-  console.log(cart);
+  const cart = useSelector((state) => state.cart.carts);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
 
   const dispatch = useDispatch();
 
-  // const cartData = cart && cart.length ? cart[0] : [];
-
   const dispatchFiltered = () => {
-    if (cart?.length > 0 && cart?.length < 1) {
-      console.log("Select cart");
-    } else {
-      // alert("You have already selected a cart");
-      dispatch(
-        addCart({
-          id,
-          title,
-          image,
-          price,
-        })
-      );
-    }
+    let itemPrice = parseFloat(price.replace(/[₦,]/g, ""));
+    dispatch(
+      addCart({
+        id,
+        title,
+        image,
+        itemPrice,
+      })
+    );
+
+    dispatch(updatePrice(itemPrice));
   };
 
   return (
@@ -33,15 +28,13 @@ function Items({ id, title, price, image }) {
       <div className="flex items-start justify-between w-full p-4">
         <div className="flex flex-col">
           <p className="font-semibold tracking-[0.065em]">{title}</p>
-          <p className="tracking-[0.065em]">
-            {price}
-          </p>
+          <p className="tracking-[0.065em]">{price}</p>
         </div>
         <button
           className="flex gap-x-2 lowercase bg-black text-white py-2 px-4 rounded-md hover:bg-secondary"
           onClick={() => dispatchFiltered()}
         >
-          <span>Add to cart</span>
+          Add to cart
         </button>
       </div>
     </div>
