0c15fe56adbc5acd9a7abed1991cc05e707beb70
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-22 00:25:44 +0100

    Update cardItems.js

--- src/components/card/cardItems.js
+++ src/components/card/cardItems.js
@@ -7,15 +7,15 @@ const CardItems = ({ id, qty, image, name, price }) => {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <img className="" src={image} alt="itemsgraphic" />
-      <div className="flex w-full justify-around">
+    <div className="flex flex-col items-center">
+      <img width={800} height={800} src={image} alt="itemsgraphic" />
+      <div className="flex w-full bg-secondary rounded-b-md justify-around">
         <div className="flex flex-col p-4 gap-y-2">
-          <h2 className="font-semibold tracking-[0.065em]">
+          <h2 className="font-semibold text-white tracking-[0.065em]">
             Ticket Name : {name}
           </h2>
-          <p className="tracking-[0.065em]">{qty}</p>
-          <p className="tracking-[0.065em]">{price}</p>
+          <p className="tracking-[0.065em] text-white">{qty}</p>
+          <p className="tracking-[0.065em] text-white">{price}</p>
         </div>
         <div className="flex mt-4 gap-3 items-start">
           <button
