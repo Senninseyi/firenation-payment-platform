3c1fea9187bead88b290e23be941d3c4373793c8
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-22 00:27:15 +0100

    Update cart.js

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CartItems from "../components/card/cardItems";
-import { Modal } from "../components/modal/modal";
 import { showModal, hideModal } from "../app/slices/modal";
 import { useNavigate } from "react-router-dom";
+
+import { PaymentForm } from "../components/form/form";
+import { Logo } from "../assets";
+import { CloseCircleOutlined } from "@ant-design/icons";
 
 const Cart = () => {
   const modal = useSelector((state) => state.modal.modals);
@@ -40,7 +43,7 @@ const Cart = () => {
   }
 
   return (
-    <div className="mt-24 flex justify-center gap-4 w-full items-center">
+    <div className="mt-24 flex justify-center flex-col gap-4 w-full items-center">
       <div className="grid w-full max-w-4xl bg-white place-items-center gap-4">
         {cart?.map((_) => (
           <CartItems
@@ -62,13 +65,36 @@ const Cart = () => {
         ))}
         <button
           onClick={() => triggerModal()}
-          className="bg-primary text-white w-fit mb-6 px-4 py-2 tracking-[0.065em] rounded-md"
+          className="bg-primary text-white w-fit px-4 py-2 tracking-[0.065em] rounded-md"
         >
           Proceed to checkout
         </button>
       </div>
 
-      {modal && <Modal open={modal} close={() => dispatch(hideModal())} />}
+      {modal && (
+        <div
+          className={
+            modal
+              ? "bg-black border-2 rounded-md shadow w-full max-w-3xl grid place-items-center"
+              : ""
+          }
+        >
+          <div className="flex w-full justify-center flex-col p-6 items-center gap-4">
+            <div className=" flex w-full justify-between  items-center">
+              <div className="bg-opacity-60 rounded-full">
+                <Logo width={50} />
+              </div>
+              <button
+                className="text-black text-2xl rounded-full leading-none p-1 flex items-center bg-secondary"
+                onClick={() => dispatch(hideModal())}
+              >
+                <CloseCircleOutlined width={36} height={36} />
+              </button>
+            </div>
+            <PaymentForm />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
