7e60f151f436d684afe2227ac196eb969b3203c6
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-22 13:05:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout";
@@ -6,27 +5,10 @@ import Cart from "./modules/cart";
 import Home from "./modules/home";
 
 function App() {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/src/serviceworker.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, []);
   return (
     <Layout>
       <Routes>
-        <Route element={<Home />} path="/" />
+        <Route element={<Home/>} path="/"/>
         <Route element={<Cart />} path="cart" />
       </Routes>
     </Layout>
