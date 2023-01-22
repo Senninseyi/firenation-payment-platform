2f4939d27ad48a6ffc86689cd7a127ff892370e0
Senninseyi <seyi.oyebamiji@gmail.com>
2023-01-22 12:23:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./app/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { register } from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -19,7 +20,7 @@ root.render(
   </BrowserRouter>
 );
 
-serviceWorker.register();
+register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
